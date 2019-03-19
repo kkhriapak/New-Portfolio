4f50e3a7af49e3c8cea7fde889663443e1bca1c0
Kateryna <kkhriapak@gmail.com>
2019-03-19 10:10:24 -0400

    Project names

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 console.log('Welcome to my portfolio site!')
 
-$(document).ready(function () {
-  $('.project-preview').mouseenter(function () {
+$(document).ready(function() {
+  $('.project-preview').mouseenter(function() {
     $(this).children('.project-details').css('opacity', '1')
     $(this).children('.fade').children('.project-image').css('opacity', '0.3')
   })
-  $('.project-preview').mouseleave(function () {
+  $('.project-preview').mouseleave(function() {
     $(this).children('.project-details').css('opacity', '0')
     $(this).children('.fade').children('.project-image').css('opacity', '1')
   })
